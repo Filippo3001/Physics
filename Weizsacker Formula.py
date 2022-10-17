ee0d8e5ef7469dea1ca61f4cbfbfc83f9ef2cbe6
Filippo Pretolani <filippopretolani88@gmail.com>
2022-10-17 20:22:23 +0200

    corrected delta function

--- Weizsacker Formula.py
+++ Weizsacker Formula.py
@@ -10,9 +10,9 @@ a_p = 34.0
 
 
 def delta(A, Z):
-    if A % 2 == 0 & Z % 2 == 0:
+    if (A - Z) % 2 == 0 & Z % 2 == 0:
         return 1
-    if A % 2 != 0 & Z % 2 != 0:
+    if (A - Z) % 2 != 0 & Z % 2 != 0:
         return -1
     else:
         return 0
@@ -21,9 +21,9 @@ def delta(A, Z):
 def B(A, Z):
     return a_v*A - a_s*(A**(2/3)) - a_l*(Z*(Z-1)/(A**(1/3))) - a_a*((A- 2*Z)**2)/A + a_p*delta(A,Z)/(A**(3/4))
 
-z = 26 # fisso z
+z = 49 # fisso z
 
-a = np.arange(45, 70)
+a = np.arange(96, 137)
 B_on_A_values = []
 for x in  a:
     B_on_A_values.append(B(x,z)/x)
