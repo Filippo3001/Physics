6e87a020fc94b49f7e21c59b018501178b947c27
Filippo Pretolani <filippopretolani88@gmail.com>
2022-11-03 20:39:46 +0100

    added nucleus class, started working on decays

--- Weizsacker Formula.py
+++ Weizsacker Formula.py
@@ -9,25 +9,53 @@ a_a = 23.0
 a_p = 34.0
 
 
-def delta(A, Z):
-    if (A - Z) % 2 == 0 & Z % 2 == 0:
+class Nucleus:
+    def __init__(self, a, z):
+        self.A = a
+        self.Z = z
+
+    def alpha(self):
+        self.A -= 4
+        self.Z -= 2
+
+    def betaminus(self):
+        self.Z += 1
+
+    def betaplus(self):
+        self.Z -= 1
+
+
+
+def delta(nuc):
+    if (nuc.A - nuc.Z) % 2 == 0 & nuc.Z % 2 == 0:
         return 1
-    if (A - Z) % 2 != 0 & Z % 2 != 0:
+    if (nuc.A - nuc.Z) % 2 != 0 & nuc.Z % 2 != 0:
         return -1
     else:
         return 0
 
 
-def B(A, Z):
-    return a_v*A - a_s*(A**(2/3)) - a_l*(Z*(Z-1)/(A**(1/3))) - a_a*((A- 2*Z)**2)/A + a_p*delta(A,Z)/(A**(3/4))
+def B(nuc):
+    return a_v * nuc.A - a_s * (nuc.A ** (2 / 3)) - a_l * (nuc.Z * (nuc.Z - 1) / (nuc.A ** (1 / 3))) - a_a * (
+            (nuc.A - 2 * nuc.Z) ** 2) / nuc.A + a_p * delta(nuc) / (nuc.A ** (3 / 4))
 
-z = 49 # fisso z
+
+def is_alpha(nuc):
+    if nuc.A < 4 or nuc.Z < 2:
+        return 1  # false
+    if B(nuc) > B(nuc.alpha()):
+        return 1  # false
+    else:
+        return 0  # true
+
+
+z = 49  # fisso z
 
 a = np.arange(96, 137)
 B_on_A_values = []
-for x in  a:
-    B_on_A_values.append(B(x,z)/x)
+for x in a:
+    nuc = Nucleus(x, z)
+    B_on_A_values.append(B(nuc) / x)
 
 plt.plot(a, B_on_A_values, "o")
 plt.show()
-
